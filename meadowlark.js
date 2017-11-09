6c58ced557214dbfa8f74a460bf46d80a912ca82
Kundan Kumar <kundan.kumar01@sap.com>
2017-11-09 08:24:43 +0530

    Moved 'fortune cookine' functonality into a module

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var fortune = require('./lib/fortune.js');
+
+
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 
@@ -17,13 +20,7 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
   //res.type('text/plain');
   //res.send('About Meadowlark Travel');
-  var fortunes = [
-      "Conquer your fears or they will conquer you.", "Rivers need springs.",
-      "Do not fear what you don't know.",
-      "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-    ];
-  var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-  res.render('about', { fortune: randomFortune});
+  res.render('about', { fortune: fortune.getFortune()});
 });
 
 //custom 404 page
